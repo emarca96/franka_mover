02caa308126d67ed1598369e72dc3a408c4383ed
Emarca96 <emanuele.arca@yahoo.it>
2025-02-04 00:29:20 +0100

    modificato script aruco_print.py

--- scripts/aruco_print.py
+++ scripts/aruco_print.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
-from cv2 import aruco
+import cv2.aruco as aruco
 
-def generate_aruco_marker(dictionary_type=aruco.DICT_6X6_250, marker_id=0, marker_size=700, output_filename="aruco_marker.png"):
+def generate_aruco_marker(dictionary_type=aruco.DICT_7X7_250, marker_id=0, marker_size=700, output_filename="aruco_marker7x7.png"):
     """
     Genera un marker ArUco e lo salva come immagine.
     
@@ -11,10 +11,12 @@ def generate_aruco_marker(dictionary_type=aruco.DICT_6X6_250, marker_id=0, marke
     :param marker_size: Dimensione del marker in pixel
     :param output_filename: Nome del file immagine di output
     """
-    aruco_dict = aruco.Dictionary_get(dictionary_type)
-    marker_image = np.zeros((marker_size, marker_size), dtype=np.uint8)
-    aruco.drawMarker(aruco_dict, marker_id, marker_size, marker_image, 1)
+    aruco_dict = aruco.getPredefinedDictionary(dictionary_type)
     
+    # Genera il marker
+    marker_image = aruco_dict.generateImageMarker(marker_id, marker_size)
+    
+    # Salva l'immagine
     cv2.imwrite(output_filename, marker_image)
     print(f"Marker ArUco ID {marker_id} salvato come {output_filename}")
 
